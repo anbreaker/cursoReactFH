00101449fd727f45ba5c955ae78bf2fd2e86c625
anbreaker <antunez19@gmail.com>
2020-10-05 18:04:16 +0200

    ejercicio 2

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+// Variables y constantes
+
+const name = 'anbreaker';
+const surname = 'anbreaker';
+
+let value = 5;
+value = 4;
+
+console.log(name, surname, value);
+
+if (true) {
+  const name = 'Peter';
+  console.log(name);
+}
+console.log(name);
