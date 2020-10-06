40efad79768d48f2a38e799eb288e0b28dba3c6c
anbreaker <antunez19@gmail.com>
2020-10-06 18:44:15 +0200

    promesas ejercicio 9

--- 02-intro-javascript/src/bases/08-imports-exports.js
+++ 02-intro-javascript/src/bases/08-imports-exports.js
@@ -1,9 +1,9 @@
 import heroes, {owners} from '../data/heroes.js';
 
-const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
-console.log(getHeroeById(2));
+export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+// console.log(getHeroeById(2));
 
-const getHeroeByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
-console.log(getHeroeByOwner('DC'));
+export const getHeroeByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+// console.log(getHeroeByOwner('DC'));
 
-console.log('Prueba importacion individual\n', owners);
+// console.log('Prueba importacion individual\n', owners);
