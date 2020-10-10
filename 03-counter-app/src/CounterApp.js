be748876a2dc5c7d64d37ea0199414ba3602b0fe
anbreaker <antunez19@gmail.com>
2020-10-10 12:13:48 +0200

    sumador restador React XD

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 // Functional Component
 
-const CounterApp = ({value}) => {
-  const handleAdd = (e) => console.log(e);
+const CounterApp = ({value = 10}) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = (e) => {
+    // setCounter((c) => c + 1);
+    setCounter(counter + 1);
+  };
+
+  const handleSubtract = () => setCounter(counter - 1);
+  const handleReset = () => setCounter(value);
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
 
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubtract}>-1</button>
     </>
   );
 };
