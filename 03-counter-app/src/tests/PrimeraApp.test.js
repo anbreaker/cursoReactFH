4ddca593277976645f26d529497b7ce1b32b2dc7
anbreaker <antunez19@gmail.com>
2020-10-14 19:22:56 +0200

    trabajando con Enzyme para test

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import {render} from '@testing-library/react';
+// import {render} from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
+import {shallow} from 'enzyme';
 
 describe('Pruebas en <PrimeraApp />', () => {
-  test('Debe mostrar el mensaje', () => {
+  // Pruebas Jest Nativo
+  // test('Debe mostrar el mensaje', () => {
+  //   const saludo = 'Hola, soy Goku';
+  //   const {getByText} = render(<PrimeraApp saludo={saludo} />);
+  //   expect(getByText(saludo)).toBeInTheDocument();
+  // });
+  //
+  // Pruebas con Enzyme
+
+  test('Debe mostrar <PrimeraApp /> correctamente ', () => {
     const saludo = 'Hola, soy Goku';
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />);
 
-    const {getByText} = render(<PrimeraApp saludo={saludo} />);
-
-    expect(getByText(saludo)).toBeInTheDocument();
+    expect(wrapper).toMatchSnapshot();
   });
 });
