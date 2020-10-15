8feec257388f2b76beb8177596dc0b6c27216097
anbreaker <antunez19@gmail.com>
2020-10-15 12:20:44 +0200

    iniciando app de gifs

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Functional Component
+
+const GifExpertApp = () => {
+  return (
+    <>
+      <h2>GifExpertApp</h2>;
+      <hr />
+    </>
+  );
+};
+
+export default GifExpertApp;
