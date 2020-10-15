7237261cd44a7697b71a7d7f06830ec2145386ec
anbreaker <antunez19@gmail.com>
2020-10-15 13:27:30 +0200

    probando inserciones

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 
 // Functional Component
 
 const GifExpertApp = () => {
+  // const categories = ['Simpsons', 'Drangon Ball', 'Futurama'];
+  const [categories, setCategories] = useState(['Simpsons', 'Drangon Ball', 'Futurama']);
+
+  const handleAdd = () => {
+    // setCategories([...categories, 'Star Wars']);
+    setCategories((categoriesItem) => [...categoriesItem, 'Star Wars']);
+  };
+
   return (
     <>
-      <h2>GifExpertApp</h2>;
+      <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
--- 04-gif-expert-app/src/index.js
+++ 04-gif-expert-app/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import GifExpertApp from './GifExpertApp';
+import './index.css';
 
 ReactDOM.render(<GifExpertApp />, document.getElementById('root'));
-
-// GifExpertApp
-// <h2>GifExpertApp</h2>;
-// <hr/>
