24986aa2737f0d1c660c908597e52d23558a34aa
anbreaker <antunez19@gmail.com>
2020-10-16 20:05:12 +0200

    gif react app

--- 03-counter-app/src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-
-import {createSerializer} from 'enzyme-to-json';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({adapter: new Adapter()});
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
