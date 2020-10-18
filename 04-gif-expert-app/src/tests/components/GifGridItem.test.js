53edced8cc29fbbd39ddbe47fa68e393d56daee1
anbreaker <antunez19@gmail.com>
2020-10-18 11:10:55 +0200

    testing GifGridItem

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -22,9 +22,26 @@ import {GifGridItem} from '../../components/GifGridItem';
 describe('Pruebas en <GifGridItem />', () => {
   const title = 'Un titulo';
   const url = 'https://localhost/algo.jpg';
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
   test('Debe mostrar el componente correctamente ', () => {
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('debe tener un parrafo con el title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('debe tener la imagen igual al url y alt de los props ', () => {
+    const img = wrapper.find('img');
+    // console.log(img.props().src);
+    expect(img.props().src).toBe(url);
+    expect(img.prop('alt')).toBe(title);
+  });
+
+  test('debe tener animate__fadeIn ', () => {
+    const div = wrapper.find('div');
+    expect(div.prop('className').includes('animate__fadeIn')).toBe(true);
   });
 });
