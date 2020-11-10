05e1f719603546279fec5827502fd8f0d7228f90
anbreaker <antunez19@gmail.com>
2020-11-10 19:57:03 +0100

    prueba en cambio del input

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -5,9 +5,18 @@ import {AddCategory} from '../../components/AddCategory';
 
 describe('Pruebas en el componente', () => {
   const setCategories = () => {};
-  let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
 
   test('debe de mostrarse correctamente', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('debe de cambiar la caja de texto', () => {
+    const input = wrapper.find('input');
+    const value = 'Hola Mundo';
+
+    input.simulate('change', {target: {value}});
+
+    expect(wrapper.find('p').text().trim()).toBe(value);
   });
 });
