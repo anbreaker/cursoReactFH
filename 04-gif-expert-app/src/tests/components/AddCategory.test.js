b8009a774a61175601b1b57f2827ae7dbf8c84e4
anbreaker <antunez19@gmail.com>
2020-11-30 19:36:56 +0100

    AddCategory probada

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -30,4 +30,20 @@ describe('Pruebas en el componente', () => {
   test('No debe de postear la informacion con submit ', () => {
     expect(setCategories).not.toHaveBeenCalled();
   });
+
+  test('Debe de llamar el setCategories y limpiar la caja de texto', () => {
+    const value = 'Hola Mundo';
+    // 1. simular el inputChange
+    wrapper.find('input').simulate('change', {target: {value}});
+
+    // 2. simular el submit
+    wrapper.find('form').simulate('submit', {preventDefault() {}});
+
+    // 3. setCategories se debe de haber llamado
+    expect(setCategories).toHaveBeenCalled();
+    expect(setCategories).toHaveBeenCalledTimes(1);
+    expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+    // 4. El valor del input debe de estar vacio
+    expect(wrapper.find('input').prop('value')).toBe('');
+  });
 });
