11f4c9094141cdadacbfc4afac3bf89ea90b7d21
anbreaker <antunez19@gmail.com>
2020-12-16 17:52:24 +0100

    useRef

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 // import {CounterWithCustomHook} from '././components/01-useState/CounterWithCustomHook';
 // import {SimpleForm} from '././components/02-useEffect/SimpleForm';
 // import {FormWithCustomHook} from '././components/02-useEffect/FormWithCustomHook';
-import {MultipleUseHooks} from '././components/03-examples/MultipleUseHooks';
+// import {MultipleUseHooks} from '././components/03-examples/MultipleUseHooks';
+import {FocusScreen} from '././components/04-useRef/FocusScreen';
 
-ReactDOM.render(<MultipleUseHooks />, document.getElementById('root'));
+ReactDOM.render(<FocusScreen />, document.getElementById('root'));
