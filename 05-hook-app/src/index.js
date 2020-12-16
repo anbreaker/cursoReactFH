1d392088a8a1e0d8f38764ab1e5cb7773681f6ff
anbreaker <antunez19@gmail.com>
2020-12-16 14:09:40 +0100

    breakingbad quote, useState, useEffect

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 // import {CounterApp} from '././components/01-useState/CounterApp';
 // import {CounterWithCustomHook} from '././components/01-useState/CounterWithCustomHook';
 // import {SimpleForm} from '././components/02-useEffect/SimpleForm';
-import {FormWithCustomHook} from '././components/02-useEffect/FormWithCustomHook';
+// import {FormWithCustomHook} from '././components/02-useEffect/FormWithCustomHook';
+import {MultipleUseHooks} from '././components/03-examples/MultipleUseHooks';
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'));
+ReactDOM.render(<MultipleUseHooks />, document.getElementById('root'));
