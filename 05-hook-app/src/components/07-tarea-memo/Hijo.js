803a3582de747c78b70948eea80769200f82b79f
anbreaker <antunez19@gmail.com>
2020-12-17 13:26:32 +0100

    ejercicio ejemplo use memo y useCalkback

--- 05-hook-app/src/components/07-tarea-memo/Hijo.js
+++ 05-hook-app/src/components/07-tarea-memo/Hijo.js
@@ -1,15 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-export const Hijo = ({ numero, incrementar }) => {
+export const Hijo = React.memo(({numero, incrementar}) => {
+  console.log('  Me volví a generar :(  ');
 
-    console.log('  Me volví a generar :(  ');
-
-    return (
-        <button
-            className="btn btn-primary mr-3"
-            onClick={ () => incrementar( numero ) }
-        >
-            { numero }
-        </button>
-    )
-}
+  return (
+    <button className="btn btn-primary m-1" onClick={() => incrementar(numero)}>
+      {numero}
+    </button>
+  );
+});
