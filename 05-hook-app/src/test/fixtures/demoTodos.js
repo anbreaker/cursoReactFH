cbefec509ec9bf16a322dfea72ccf894fc9de84f
anbreaker <antunez19@gmail.com>
2021-01-06 20:16:15 +0100

    pruebas en reducer

---
+++ 05-hook-app/src/test/fixtures/demoTodos.js
@@ -0,0 +1,12 @@
+export const demoTodos = [
+  {
+    id: 1,
+    description: 'Aprender React',
+    done: false,
+  },
+  {
+    id: 2,
+    description: 'Aprender Bases de Datos',
+    done: false,
+  },
+];
