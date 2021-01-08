9f39ec3f41a0f781b3f429ef2a77b94e4ecadf81
anbreaker <antunez19@gmail.com>
2021-01-08 18:13:37 +0100

    useMemo

--- 07-heroes-app/src/components/heroes/HeroList.js
+++ 07-heroes-app/src/components/heroes/HeroList.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {getHeroByPublisher} from '../../selectors/getHeroByPublisher';
 import {HeroCard} from './HeroCard';
 
 export const HeroList = ({publisher}) => {
-  const heroes = getHeroByPublisher(publisher);
+  // const heroes = getHeroByPublisher(publisher);
+  //useMemo, cambia si lo hace el publisher
+  const heroes = useMemo(() => getHeroByPublisher(publisher), [publisher]);
 
   return (
     <div className="card-columns">
