5e5477c37c1d04074e07cd19deb7608f60859054
anbreaker <antunez19@gmail.com>
2021-01-08 18:05:58 +0100

    estilos HeroesPage

--- 07-heroes-app/src/components/heroes/HeroesPage.js
+++ 07-heroes-app/src/components/heroes/HeroesPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Redirect, useParams} from 'react-router-dom';
 import {getHeroById} from '../../selectors/getHeroById';
 
-export const HeroesPage = () => {
+export const HeroesPage = ({history}) => {
   const {heroeId} = useParams();
 
   const hero = getHeroById(heroeId);
@@ -11,11 +11,44 @@ export const HeroesPage = () => {
     return <Redirect to="/" />;
   }
 
+  const handleReturn = () => {
+    if (history.length <= 2) history.push('/');
+    else history.goBack();
+  };
+
   const {id, superhero, publisher, alter_ego, first_appearance, characters} = hero;
 
   return (
-    <div>
-      <h1>HeroesPage</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-groupitem">
+            <b>Alter Ego: {alter_ego} </b>
+          </li>
+          <li className="list-groupitem">
+            <b>publisher: {publisher} </b>
+          </li>
+          <li className="list-groupitem">
+            <b>First Appearance: {first_appearance} </b>
+          </li>
+        </ul>
+
+        <h5>
+          <b>Characters</b>
+        </h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
