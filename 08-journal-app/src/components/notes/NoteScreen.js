92f3decc09dcfe70d91292e08fc4eec7ed51daf6
anbreaker <antunez19@gmail.com>
2021-01-11 20:59:41 +0100

    Estructura y diseño del HomeScreen

--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+
+      <div className="notes__content">
+        <input
+          className="notes__title-input"
+          type="text"
+          placeholder="Some awesome Title"
+          autoComplete="off"
+        />
+
+        <textarea
+          placeholder="What happened today"
+          className="notes__textarea"></textarea>
+
+        <div className="notes__image">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/000/246/312/non_2x/mountain-lake-sunset-landscape-first-person-view-vector.jpg"
+            alt="imagen"
+          />
+        </div>
+      </div>
     </div>
   );
 };
