6291f21bc20f31ca452d16a7d8466911f12a44c1
anbreaker <antunez19@gmail.com>
2021-01-12 14:02:01 +0100

    ejemplos de trabajo firebase

--- firebaseComplemento/webpack-starter-typescript-firestore/src/index.ts
+++ firebaseComplemento/webpack-starter-typescript-firestore/src/index.ts
@@ -1,1 +1,34 @@
-import './firebase/config';
+import db from './firebase/config';
+
+const user = {
+  name: 'Curro',
+  active: false,
+  data: 25,
+};
+
+const userRef = db.collection('users');
+// insert into users
+// db.collection('users')
+//   .add(user)
+//   .then((docRef) => {
+//     console.log(docRef.id);
+//   })
+//   .catch((error) => console.log('error', error));
+
+// userRef.doc('UPjVLD1hSl39Si9L54P0').update({
+//   active: false,
+//   name: 'Magali',
+// })
+
+// Modifica la coleccion desturyendo lo que hubiera
+// userRef.doc('UPjVLD1hSl39Si9L54P0').set({
+//   active: false,
+//   name: 'Magali',
+// });
+
+// delete from users where id = 'xx'
+// userRef
+//   .doc('AZSx13PEI1E1lRw1dxKw')
+//   .delete()
+//   .then(() => console.log('delete'))
+//   .catch((error) => console.log('error', error));
