a16349d84a3b34c8a8a13e5d9134102015416d8d
anbreaker <antunez19@gmail.com>
2021-01-13 17:10:58 +0100

    manteniendo el state...

--- 08-journal-app/src/routers/AppRouter.js
+++ 08-journal-app/src/routers/AppRouter.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-import {JournalScreen} from '../components/journal/JournalScreen';
+import {useDispatch} from 'react-redux';
+import {firebase} from '../firebase/firebaseConfig';
 
+import {JournalScreen} from '../components/journal/JournalScreen';
 import {AuthRouter} from './AuthRouter';
+import {login} from '../actions/auth';
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+        console.log(user.uid, user.displayName);
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
       <div>
