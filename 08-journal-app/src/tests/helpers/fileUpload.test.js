e2236cd6e25e002dc64d597fe74c39fedaac8ba7
anbreaker <antunez19@gmail.com>
2021-02-10 18:18:25 +0100

    Cloudinary SDK - Delete image, con error en async (done)

--- 08-journal-app/src/tests/helpers/fileUpload.test.js
+++ 08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -1,7 +1,14 @@
+import cloudinary from 'cloudinary';
 import {fileUpload} from '../../helpers/fileUpload';
 
+cloudinary.config({
+  cloud_name: process.env.REACT_APP_CLOUD_NAME,
+  api_key: process.env.REACT_APP_API_KEY,
+  api_secret: process.env.REACT_APP_API_SECRET,
+});
+
 describe('pruebas en fileUpload', () => {
-  test('debe cargar un archivo y retornar el URL', async () => {
+  test('debe cargar un archivo y retornar el URL', async (done) => {
     const imgUrl =
       'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png';
     const response = await fetch(imgUrl);
@@ -13,6 +20,14 @@ describe('pruebas en fileUpload', () => {
     const url = await fileUpload(file);
 
     expect(typeof url).toBe('string');
+
+    //Borrar imagen por id
+    const segments = url.split('/');
+    const imageId = segments[segments.length - 1].replace('.png', '');
+
+    cloudinary.v2.api.delete_resources(imageId, {}, () => {
+      done();
+    });
   });
 
   test('debe retornar un error', async () => {
