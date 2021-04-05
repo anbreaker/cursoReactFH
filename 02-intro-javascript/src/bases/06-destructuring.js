6c2f12f63ffd231d95ff25695851e8e5011a8f07
anbreaker <antunez19@gmail.com>
2021-04-05 12:41:18 +0200

    nuevo ejemplo destructuracion con ejemplos de funciones en object deadpool

--- 02-intro-javascript/src/bases/06-destructuring.js
+++ 02-intro-javascript/src/bases/06-destructuring.js
@@ -17,17 +17,17 @@ console.log(person.secret);
 // console.log(secret);
 
 const returnPerson = (user) => {
-  const {name, surname, secret} = user;
+  const { name, surname, secret } = user;
   console.log(name, surname, secret);
 };
 returnPerson(person);
 
-const returnPerson2 = ({surname, rango = 'boss'}) => {
+const returnPerson2 = ({ surname, rango = 'boss' }) => {
   console.log(surname, rango);
 };
 returnPerson2(person);
 
-const useContext = ({secret, name, age}) => {
+const useContext = ({ secret, name, age }) => {
   return {
     keyName: name,
     years: age,
@@ -42,7 +42,25 @@ const {
   keyName,
   years,
   rango,
-  latlng: {lat, lng},
+  latlng: { lat, lng },
 } = useContext(person);
 console.log(keyName, years, rango);
 console.log(lat, lng);
+
+const deadpool = {
+  name: 'Wade',
+  surname: 'Winston',
+  pow: 'Regeneration',
+  getName: function () {
+    return `\n${this.name} ${this.surname}`;
+  },
+  getPow() {
+    return `poder: ${this.pow}`;
+  },
+};
+
+console.log(deadpool.getName(), deadpool.getPow());
+
+const { name, surname, pow } = deadpool;
+
+console.log(name, surname, pow);
